4ffa91a1fb42ce8c9b31e934799997c03c022284
stephanielapierre <setphanie.lapierre@usherbrooke.ca>
2021-12-07 08:38:35 -0500

    exploration file

--- R/data-download.R
+++ R/data-download.R
@@ -3,19 +3,19 @@
 ## Our world in data 
 # Données sur la vaccination
 data_URL <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
-write.csv(data_URL, file = paste('data/raw/vaccination_', Sys.Date(), '.csv'))
+write.csv(data_URL, file = paste('data/raw/vaccination_', Sys.Date(), '.csv', sep = ""))
 
 ## Gapminder
 # Poupulation
 download.file("https://docs.google.com/spreadsheets/d/14_suWY8fCPEXV0MH7ZQMZ-KndzMVsSsA5HdR-7WqAC0/export?format=xlsx", 
-              destfile = paste('data/raw/population_', Sys.Date(), '.xlsx'))
+              destfile = paste('data/raw/population_', Sys.Date(), '.xlsx', sep = ""))
 
 # Produit intérieur brut (PIB)
 download.file("https://docs.google.com/spreadsheets/d/1h3z8u0ykcUum8P9FV8EHF9fszDYr7iPDZQ-fgE3ecls/export?format=xlsx", 
-              destfile = paste('data/raw/life-expectancy_', Sys.Date(), '.xlsx'))
+              destfile = paste('data/raw/life-expectancy_', Sys.Date(), '.xlsx', sep = ""))
 
 # Espérance de vie
 download.file("https://docs.google.com/spreadsheets/d/11mulzUH3_cueq-V9D5KIlo9oHE9YYZrUSeVyCin7_rM/export?format=xlsx", 
-              destfile = paste('data/raw/life-expectancy_', Sys.Date(), '.xlsx'))
+              destfile = paste('data/raw/life-expectancy_', Sys.Date(), '.xlsx', sep = ""))
 
-
+filenames <- list.files("data/raw/", pattern ="vaccination")
