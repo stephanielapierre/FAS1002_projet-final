321b4d03db3703429af64c5ff4156f301860dd27
stephanielapierre <setphanie.lapierre@usherbrooke.ca>
2021-12-12 17:50:15 -0500

    script enregistrement df vaccination

--- R/processed-data.R
+++ R/processed-data.R
@@ -1,14 +1,34 @@
-### Script pour enregistrer les données traitées
+### Script pour enregistrer les données traitées ###
+
+
+## Jeu de données 2021, une ligne par pays/continent ##
 
 # Si on fait des corrections dans les données et qu'on veut enregister
-# write.csv(df2021, paste0('data/processed/data_', Sys.Date(), '.csv'))
+# write.csv(df2021, paste0('data/processed/data_year_', Sys.Date(), '.csv'))
 
 # Extraction de la date dernier enregistrement des données
-processed_file <- list.files("data/processed/", pattern ="data")
-date_processed_file <- str_sub(processed_file, 6, 15)
+processed_file <- list.files("data/processed/", pattern ="data_year")
+date_processed_file <- str_sub(processed_file, 11, 20)
 
 # Conditionnel pour vérifier les données doivent être enregistrées
 if (date_processed_file < Sys.Date()){
-    write.csv(df2021, paste0('data/processed/data_', Sys.Date(), '.csv'))
-    file.remove(paste0('data/processed/data_', date_processed_file, '.csv'))         # On enleve les données old
+    write.csv(df2021, paste0('data/processed/data_year_', Sys.Date(), '.csv'))
+    file.remove(paste0('data/processed/data_year_', date_processed_file, '.csv'))         # On enleve les données old
+}
+
+
+## Jeu de données basé sur la vaccination, données quotidienne ##
+
+# Si on fait des corrections dans les données et qu'on veut enregister
+# write.csv(df, paste0('data/processed/df_', Sys.Date(), '.csv'))
+
+
+# Extraction de la date dernier enregistrement des données
+processed_file <- list.files("data/processed/", pattern ="df")
+date_processed_file <- str_sub(processed_file, 4, 13)
+
+# Conditionnel pour vérifier les données doivent être enregistrées
+if (date_processed_file < Sys.Date()){
+    write.csv(df, paste0('data/processed/df_', Sys.Date(), '.csv'))
+    file.remove(paste0('data/processed/df_', date_processed_file, '.csv'))         # On enleve les données old
 }
